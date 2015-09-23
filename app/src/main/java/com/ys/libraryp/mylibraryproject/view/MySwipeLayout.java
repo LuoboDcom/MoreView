cdc3709657da1da6ae44b90f18428b3d74d87876
yueshan <yueshan0607@163.com>
2015-09-23 12:03:41 +0800

    update

---
+++ app/src/main/java/com/ys/libraryp/mylibraryproject/view/MySwipeLayout.java
@@ -0,0 +1,21 @@
+package com.ys.libraryp.mylibraryproject.view;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+
+/**
+ * Created by Administrator on 2015/9/23 0023.
+ */
+public class MySwipeLayout extends SwipeRefreshLayout{
+    public MySwipeLayout(Context context) {
+        super(context);
+    }
+
+    public MySwipeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    on
+
+}
